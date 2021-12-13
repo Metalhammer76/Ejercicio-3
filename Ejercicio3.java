4a7a5c3761996cb80c22f8c57e233d159c2ce9a2
Metalhammer76 <95860404+Metalhammer76@users.noreply.github.com>
2021-12-13 13:09:49 +0100

    Add files via upload

---
+++ Ejercicio3.java
@@ -0,0 +1,30 @@
+
+public class Main{
+
+	public static void main(String[] args) {
+	    
+	    /*
+	    Crear bucle que permita concatenar texto
+	    
+	    String FreddieMercury = "Freddie Mercury";
+	    String BrianMay = "Brian May";
+	    String RogerTaylor = "Roger Taylor";
+	    String JohnDeacon = "John Deacon";
+	    
+	    String Queen = "Queen";
+	    
+	    String[] nombres = {"", "", "", ""}
+	    */
+	    
+	    String FreddieMercury = "Freddie Mercury";
+	    String BrianMay = "Brian May";
+	    String RogerTaylor = "Roger Taylor";
+	    String JohnDeacon = "John Deacon";
+	    
+	    String Queen = "Queen";
+	    
+	    System.out.println("Los 4 miembros de"+ "Queen" + " son :" + FreddieMercury + ","  + BrianMay + "," + RogerTaylor + " y " + JohnDeacon );
+	    
+	}
+}
+
